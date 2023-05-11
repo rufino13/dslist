739266c41ee1162d6137b726e542f5ce676dda29
Igor Alves <igorhra92@gmail.com>
2023-05-11 17:31:57 -0300

    search by list

--- src/main/java/com/devsuperior/dslist/DTO/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/DTO/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.DTO;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -15,12 +16,19 @@ public class GameMinDTO {
 	}
 
 	public GameMinDTO(Game entity) {
-		super();
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
